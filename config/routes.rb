38c01dc7d904cb0f86e476b9d3b66e64c6210b4e
Blake.Grabsky <Blake.Grabsky@medecision.com>
2019-12-02 10:07:08 -0600

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  #get localhost:3000/'......', to: 'controller#function_in_controller'
   get 'about', to: 'pages#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
