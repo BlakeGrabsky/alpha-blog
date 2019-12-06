4f383090b99f381667197fad7c15e6a87d38db3a
Blake.Grabsky <Blake.Grabsky@medecision.com>
2019-12-06 12:03:46 -0600

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
 
 	#all articles belong to one user
 	belongs_to :user
+	#all articles have present in one or more tuple in the ArticleCategory table
+	has_many :article_categories
+	#all articles have one or more categories, defined by the tuple(s) in the ArticleCategory table
+	has_many :categories, through: :article_categories
 	
 	validates :title, 
 		presence: true,
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
+	
+	#all categories can be found in multiple tuples of the ArticleCategory table
+	has_many :article_categories
+	#all categories can be assigned to many articles
+	has_many :articles, through: :article_categories
+
 	validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 	validates_uniqueness_of :name
 end
